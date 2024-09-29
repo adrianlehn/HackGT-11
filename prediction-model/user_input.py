27cd84fac4b3d0e6c9422f795d0b613866359074
adrianlehn <lehnhaeuseradrian@gmail.com>
2024-09-28 22:27:58 -0400

    final touches on user_input.py

--- prediction-model/user_input.py
+++ prediction-model/user_input.py
@@ -17,10 +17,12 @@ def preprocess_user(user_data, scaler, feature_names):
         if col not in df.columns:
             df[col] = 0
     df = df[feature_names]
+
+    for col in df.columns:
+        print('Feature is: ',col, ", attribute: ", df[col])
     
     # numerical_cols = df.select_dtypes(include=['int64']).columns
     # df[numerical_cols] = scaler.transform(df[numerical_cols])
-    print(df.columns)
     return df
 
 def predict_readmission(model, user_input):
@@ -33,20 +35,20 @@ if __name__ == "__main__":
     log_reg_model, scaler, feature_names = load_model('prediction-model/logistic_regression_model.pkl', 'prediction-model/scaler.pkl', 'prediction-model/feature_names.pkl')
     user_input_data = {
         'age': '[70-80)',
-        'time_in_hospital': 20,
-        'n_lab_procedures': 0,
-        'n_procedures': 17,
+        'time_in_hospital': 30,
+        'n_lab_procedures': 50,
+        'n_procedures': 0,
         'n_medications': 0,
         'n_outpatient': 0,
-        'n_inpatient': 1,
+        'n_inpatient': 0,
         'n_emergency': 0,
         'medical_specialty': 'Missing',
         'diag_1': 'Circulatory',
         'diag_2': 'Respiratory',
         'diag_3': 'Respiratory',
         'glucose_test': 'high',
-        'A1Ctest': 'no',
-        'change': 'no',
+        'A1Ctest': 'high',
+        'change': 'yes',
         'diabetes_med': 'yes',
     }
     preprocessed_input = preprocess_user(user_input_data, scaler, feature_names)
