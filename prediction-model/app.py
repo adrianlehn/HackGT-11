b6c9cf93e76e5ab2722817e241447cadd2f36c07
adrianlehn <lehnhaeuseradrian@gmail.com>
2024-09-29 04:23:35 -0400

    created app.py for flask backend

---
+++ prediction-model/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+log_reg_model, scaler, feature_names = joblib.load('prediction-model/logistic_regression_model.pkl'), \
+                                       joblib.load('prediction-model/scaler.pkl'), \
+                                       joblib.load('prediction-model/feature_names.pkl')
+
+def preprocess_user(user_data, scaler, feature_names):
+    df = pd.DataFrame([user_data])
+
+    categorical_cols = df.select_dtypes(include=['object']).columns
+    df = pd.get_dummies(df, columns=categorical_cols, drop_first=False)
+    
+    for col in feature_names:
+        if col not in df.columns:
+            df[col] = 0
+    df = df[feature_names]
+        
+    # numerical_cols = df.select_dtypes(include=['int64']).columns
+    # df[numerical_cols] = scaler.transform(df[numerical_cols])
+    return df
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    user_data = request.json
+    preprocessed_input = preprocess_user(user_data, scaler, feature_names)
+    probability = log_reg_model.predict_proba(preprocessed_input)[:,1][0]
+
+    return jsonify({'prediction': probability})
+
+if __name__ == '__main__':
+    app.run(debug=True)
