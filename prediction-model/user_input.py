b53c081068d15b0dfd7290c520916f04e2ec9f6f
adrianlehn <lehnhaeuseradrian@gmail.com>
2024-09-29 01:36:02 -0400

    made user_input.py terminal interactive

--- prediction-model/user_input.py
+++ prediction-model/user_input.py
@@ -17,10 +17,7 @@ def preprocess_user(user_data, scaler, feature_names):
         if col not in df.columns:
             df[col] = 0
     df = df[feature_names]
-
-    for col in df.columns:
-        print('Feature is: ',col, ", attribute: ", df[col])
-    
+        
     # numerical_cols = df.select_dtypes(include=['int64']).columns
     # df[numerical_cols] = scaler.transform(df[numerical_cols])
     return df
@@ -33,26 +30,35 @@ def predict_readmission(model, user_input):
 
 if __name__ == "__main__":
     log_reg_model, scaler, feature_names = load_model('prediction-model/logistic_regression_model.pkl', 'prediction-model/scaler.pkl', 'prediction-model/feature_names.pkl')
+
     user_input_data = {
-        'age': '[70-80)',
-        'time_in_hospital': 30,
-        'n_lab_procedures': 50,
+        'age': '[40-50)',
+        'time_in_hospital': 2,
+        'n_lab_procedures': 51,
         'n_procedures': 0,
-        'n_medications': 0,
+        'n_medications': 10,
         'n_outpatient': 0,
         'n_inpatient': 0,
         'n_emergency': 0,
         'medical_specialty': 'Missing',
-        'diag_1': 'Circulatory',
-        'diag_2': 'Respiratory',
-        'diag_3': 'Respiratory',
+        'diag_1': 'Other',
+        'diag_2': 'Other',
+        'diag_3': 'Other',
         'glucose_test': 'high',
         'A1Ctest': 'high',
         'change': 'yes',
         'diabetes_med': 'yes',
     }
+
+    for key in user_input_data.keys():
+        if key in ['time_in_hospital','n_lab_procedures','n_procedures','n_medications','n_outpatient','n_inpatient','n_emergency']:
+            user_input_data[key] = int(input(f"Enter value for {key} (numeric): "))
+        else:
+            user_input_data[key] = input(f"Enter value for {key} (text): ")
+
+
     preprocessed_input = preprocess_user(user_input_data, scaler, feature_names)
-    probability = predict_readmission(log_reg_model, preprocessed_input)
+    probability = predict_readmission(log_reg_model, preprocessed_input)[0]
     print('Predicted readmission within 30 days:')
     print(probability)
     
